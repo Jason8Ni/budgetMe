f4ee3763d6314dd4da3a6475e52b90d7d84bea53
Jason Ni <jason8ni@hotmail.com>
2018-03-26 22:35:35 -0400

    finished user backend routing

--- services/budgetAPI/app/routers/user.js
+++ services/budgetAPI/app/routers/user.js
@@ -2,13 +2,16 @@ const models = require('@Budget/app/setup'),
     passport = require('passport'),
     config = require('@config');
 
-    module.exports = (app) => {
-        const api = app.budgetAPI.app.api.authentication;
-        
-        app.route('/api/v1/setup')
-        .post(api.setup(models.User))
+module.exports = (app) => {
+    const api = app.budgetAPI.app.api.authentication;
 
-        
-    
-    
-    }    
+    app.route('/api/v1/setup')
+        .post(api.createUser(models.User))
+
+    app.route('/api/v1/users')
+        .get(passport.authenticate('jwt', config.session),
+        api.index(models.User, app.get('mySecret')));
+
+    app.route('/api/v1/signup')
+        .post(api.signup(models.User));
+}    
