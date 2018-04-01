939f554c19ded9a9de02367122c89e66c5ef417f
Jason Ni <jason8ni@hotmail.com>
2018-03-31 23:39:10 -0400

    new routing file structure

---
+++ app/routes/authentication.js
@@ -0,0 +1,26 @@
+var express = require("express"),
+	router = express.Router(),
+	bodyParser = require("body-parser"),
+	parseUrlencoded = bodyParser.urlencoded({ extended:false }),
+	async = require('async'),
+    pool = process.pool
+    
+import Authentication from '@/components/pages/Authentication/Authentication'
+
+// Session Middleware
+router.use(function(req,res,next) {
+  	next()
+})
+
+router.route("/")
+	.get(function(req,res) {
+		res.render('home/index', {
+			ADDRESS:process.env.ADDRESS,
+			PORT:process.env.PORT,
+			ENV:process.env.ENV,
+			MINIFY:process.env.MINIFY
+		})
+
+	})
+
+module.exports = router
