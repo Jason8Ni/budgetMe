fe36ef4682c0ae9b3c54b1cdcf347727445691a4
Jason Ni <jason8ni@hotmail.com>
2018-04-02 21:35:45 -0400

    authentication component methods

--- app/src/components/pages/Authentication/index.js
+++ app/src/components/pages/Authentication/index.js
@@ -1,7 +1,48 @@
 import Axios from 'axios'
 import router from '@/router'
-const BudgetManagerAPI = `http://${window.location.hostname}:3001`
+import config from '@config'
+const url = `http://localhost:3001`
+const version = 'v1'
 
 export default {
-    user: { authenticated: false }
-  }
+  user: { authenticated: false },
+
+  authenticate(component, auth, redirect) {
+    Axios.post(`${api}/api/${version}/auth`, auth)
+      .then(({ data: { token } }) => {
+        component.$cookie.set('token', token, '2D')
+        component.validLogin = true
+        this.user.authenticated = true
+
+        if (redirect) { router.push(redirect) }
+
+      }).catch(({ response: { data } }) => {
+        component.snackbar = true
+        component.message = data.message
+      })
+  },
+
+  signup(component, auth, redirect) {
+    Axios.post(`${api}/api/${version}/signup`, auth)
+      .then(({ data: { token } }) => {
+        component.$cookie.set('token', token, '2D')
+        component.validSignUp = true
+        this.user.authenticated = true
+
+        if (redirect) { router.push(redirect) }
+      }).catch(({ response: { data } }) => {
+        context.snackbar = true
+        context.message = data.message
+      })
+  },
+
+  isAuthenticated() {
+    const token = document.cookie
+
+    if (token)
+      this.user.authenticated = true
+    else
+      this.user.authenticated = false
+  }, 
+
+}
